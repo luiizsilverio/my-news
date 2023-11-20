976962fbb578260a5c21d1963002f2251d336bba
luiizsilverio <luiiz.silverio@gmail.com>
2023-11-20 20:56:31 -0300

    mongoDB

--- server/index.js
+++ server/index.js
@@ -3,13 +3,24 @@ dotenv.config();
 
 import cors from "cors";
 import express from "express";
+import connectDB from "./connectDB.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+connectDB();
+
 app.get("/", (req, res) => {
   res.send("My-News API");
+})
+
+app.get("*", (req, res) => {
+  return res.sendStatus(404);
 })
 
 app.listen(PORT, () => {
